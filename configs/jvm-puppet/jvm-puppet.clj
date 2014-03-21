5c12306e2d45017596905e88b5085dcac2272de5
Chris Price <chris@puppetlabs.com>
2014-03-21 13:22:30 -0700

    Tweak jvm-puppet settings so that they will work with packages

--- configs/jvm-puppet/jvm-puppet.clj
+++ configs/jvm-puppet/jvm-puppet.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs.packages/jvm-puppet "0.0.2-SNAPSHOT"
+(defproject puppetlabs.packages/jvm-puppet "0.1.0-SNAPSHOT"
   :description "Release artifacts for jvm-puppet"
   :pedantic? :warn
   :dependencies [[puppetlabs/jvm-puppet "0.1.0-SNAPSHOT"]
