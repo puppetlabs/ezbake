776ad70509480dd9e96b143fe00446c6ecf11421
Matthaus Owens <matthaus@puppetlabs.com>
2014-04-09 11:29:15 -0700

    (maint) Update version to SNAPSHOT
    Now that 0.1.0 has been produced, the version must be updated to a SNAPSHOT
    version to allow new builds to commence.

--- configs/jvm-puppet/jvm-puppet.clj
+++ configs/jvm-puppet/jvm-puppet.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs.packages/jvm-puppet "0.1.0"
+(defproject puppetlabs.packages/jvm-puppet "0.1.1-SNAPSHOT"
   :description "Release artifacts for jvm-puppet"
   :pedantic? :warn
   :dependencies [[puppetlabs/jvm-puppet "0.1.0"]
