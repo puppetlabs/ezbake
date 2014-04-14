8d6e2b9f623dee14ac4343398b95c84065f0915f
Matthaus Owens <matthaus@puppetlabs.com>
2014-04-14 16:37:50 -0700

    (PE-3177) Add pe-jvm-puppet config
    This commit copies and updates the jvm-puppet config to pe-jvm-puppet.
    It updates the user and group for the project and updates pathing to
    match PE standards.

---
+++ configs/pe-jvm-puppet/pe-jvm-puppet.clj
@@ -0,0 +1,17 @@
+(defproject puppetlabs.packages/pe-jvm-puppet "0.1.1-SNAPSHOT"
+  :description "Release artifacts for pe-jvm-puppet"
+  :pedantic? :warn
+  :dependencies [[puppetlabs/jvm-puppet "0.1.0"]
+                 [puppetlabs/trapperkeeper-webserver-jetty9 "0.3.4"]]
+
+  :uberjar-name "jvm-puppet-release.jar"
+
+  :repositories [["releases" "http://nexus.delivery.puppetlabs.net/content/repositories/releases/"]
+                 ["snapshots" "http://nexus.delivery.puppetlabs.net/content/repositories/snapshots/"]]
+  :main puppetlabs.trapperkeeper.main
+
+  :ezbake { :user "pe-puppet"
+            :group "pe-puppet"
+            :build-type "pe"
+            }
+  )
