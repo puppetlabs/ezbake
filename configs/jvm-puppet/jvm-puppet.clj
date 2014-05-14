d139553be7f47917de09625ad22d0f3a1bec7096
Wayne <wayne@puppetlabs.com>
2014-05-13 18:00:59 -0700

    Update jvm-puppet project configs.
    jvm-puppet project configs now take jvm-puppet-version (pe-jvm-puppet-version
    for pe-jvm-puppet) key=value parameters to simplify version specification in CI
    and development.
    
    Signed-off-by: Wayne <wayne@puppetlabs.com>

--- configs/jvm-puppet/jvm-puppet.clj
+++ configs/jvm-puppet/jvm-puppet.clj
@@ -1,9 +1,7 @@
-(def jvm-puppet-version (or (System/getenv "JVMPUPPET_NEXUS_VERSION") "0.1.2-SNAPSHOT"))
-
-(defproject puppetlabs.packages/jvm-puppet jvm-puppet-version
+(defproject puppetlabs.packages/jvm-puppet "{{{jvm-puppet-version}}}"
   :description "Release artifacts for jvm-puppet"
   :pedantic? :abort
-  :dependencies [[puppetlabs/jvm-puppet ~jvm-puppet-version]
+  :dependencies [[puppetlabs/jvm-puppet "{{{jvm-puppet-version}}}"]
                  [puppetlabs/trapperkeeper-webserver-jetty9 "0.5.1"]]
 
   :uberjar-name "jvm-puppet-release.jar"
