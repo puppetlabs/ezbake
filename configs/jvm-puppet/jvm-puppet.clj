e7af818b337d8e93ab66b8fe767ca2d62bc0b656
Jeremy Barlow <jeremy.barlow@puppetlabs.com>
2014-05-31 16:14:30 -0600

    Updated JVMP Jetty9 dependency to 0.5.2
    This commit updates the jvm-puppet project's dependency on
    trapperkeeper-webservices-jetty9 to version 0.5.2.

--- configs/jvm-puppet/jvm-puppet.clj
+++ configs/jvm-puppet/jvm-puppet.clj
@@ -2,7 +2,7 @@
   :description "Release artifacts for jvm-puppet"
   :pedantic? :abort
   :dependencies [[puppetlabs/jvm-puppet "{{{jvm-puppet-version}}}"]
-                 [puppetlabs/trapperkeeper-webserver-jetty9 "0.5.1"]]
+                 [puppetlabs/trapperkeeper-webserver-jetty9 "0.5.2"]]
 
   :uberjar-name "jvm-puppet-release.jar"
 
