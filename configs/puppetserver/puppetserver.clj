9db94f518b9959f4a5c35c4b1bdf963b75d8bd2a
Chris Price <chris@puppetlabs.com>
2014-10-30 17:51:55 -0700

    Revert "(SERVER-29) exclude BouncyCastle jars from uberjar"
    This reverts commit 121215a730e649578c1283c62d6762904684a649.

--- configs/puppetserver/puppetserver.clj
+++ configs/puppetserver/puppetserver.clj
@@ -6,11 +6,6 @@
 
   :uberjar-name "puppet-server-release.jar"
 
-  ;; JRuby bundles the (un-exploded) BouncyCastle .jars.
-  ;; We don't want them in our uberjar,
-  ;; since we define our own dependency on BouncyCastle.
-  :uberjar-exclusions [#"META-INF/jruby.home/lib/ruby/shared/org/bouncycastle"]
-
   :repositories [["releases" "http://nexus.delivery.puppetlabs.net/content/repositories/releases/"]
                  ["snapshots" "http://nexus.delivery.puppetlabs.net/content/repositories/snapshots/"]]
   :main puppetlabs.trapperkeeper.main
