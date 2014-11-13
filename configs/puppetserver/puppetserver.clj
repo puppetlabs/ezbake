9bc43efeace7b6a97728d4fcdebb4b696282da22
Wayne <wayne@puppetlabs.com>
2014-11-13 14:26:18 -0800

    (SERVER-166) Set START_TIMEOUT to 120 seconds for puppet-server

--- configs/puppetserver/puppetserver.clj
+++ configs/puppetserver/puppetserver.clj
@@ -12,6 +12,7 @@
 
   :ezbake { :user "puppet"
             :group "puppet"
+            :start-timeout "120"
             :build-type "foss"
             :java-args "-Xms2g -Xmx2g -XX:MaxPermSize=256m"
             }
