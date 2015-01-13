094bed18917fe9c5bba6f86ad70cd8d3a323df44
Wayne <wayne@puppetlabs.com>
2015-01-13 15:23:39 -0800

    (MAINT) Add deploy-info function and deploy-repositories.

--- project.clj
+++ project.clj
@@ -1,3 +1,10 @@
+(defn deploy-info
+  [url]
+  { :url url
+    :username :env/nexus_jenkins_username
+    :password :env/nexus_jenkins_password
+    :sign-releases false })
+
 (defproject puppetlabs/lein-ezbake "0.1.0-SNAPSHOT"
   :description "A system for building packages for trapperkeeper-based applications"
   :dependencies [[me.raynes/fs "1.4.6" :exclusions [org.clojure/clojure]]
@@ -8,6 +15,8 @@
   :repositories [["releases" "http://nexus.delivery.puppetlabs.net/content/repositories/releases/"]
                  ["snapshots" "http://nexus.delivery.puppetlabs.net/content/repositories/snapshots/"]]
 
+  :deploy-repositories [["releases" ~(deploy-info "http://nexus.delivery.puppetlabs.net/content/repositories/releases/")]
+                        ["snapshots" ~(deploy-info "http://nexus.delivery.puppetlabs.net/content/repositories/snapshots/")]]
   :resource-paths ["resources/"]
 
   :profiles {:dev {:dependencies [[io.aviso/pretty "0.1.10"]]}}
