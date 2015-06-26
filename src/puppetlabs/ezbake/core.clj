6e829bbca528af15fb701899c2d9c556dbc568e6
Rob Braden <bradejr@puppetlabs.com>
2015-06-26 11:25:26 -0700

    (re-4902) Update startup timeout default to 180 seconds
    Currently the startup timeout default is set to 60 seconds. There
    have been some reports of services timing out on restart,
    particularly on lower-powered VMs. console-services already overrides
    the default to 180 seconds.
    
    This updates the default to 180 seconds.

--- src/puppetlabs/ezbake/core.clj
+++ src/puppetlabs/ezbake/core.clj
@@ -404,7 +404,7 @@ Dependency tree:
          :terminus-map              termini
          :replaces-pkgs             (get-local-ezbake-var lein-project :replaces-pkgs [])
          :start-after               (quoted-list (get-local-ezbake-var lein-project :start-after []))
-         :start-timeout             (get-local-ezbake-var lein-project :start-timeout "60")
+         :start-timeout             (get-local-ezbake-var lein-project :start-timeout "180")
          :main-namespace            (get-local-ezbake-var lein-project
                                                           :main-namespace
                                                           "puppetlabs.trapperkeeper.main")
