7ff0af9f4f89f7fb6df0db9af02fc2ee2946cde3
Jenkins CI <ci@puppetlabs.com>
2016-03-18 12:23:57 -0700

    Version 0.2.11

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/lein-ezbake "0.2.11-SNAPSHOT"
+(defproject puppetlabs/lein-ezbake "0.2.11"
   :description "A system for building packages for trapperkeeper-based applications"
   :url "https://github.com/puppetlabs/ezbake"
   :license {:name "Apache License 2.0"
