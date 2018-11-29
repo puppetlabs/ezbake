a72907ca3a0a37c60c0c00a2b50b841f2be8dc3f
Nick Lewis <nick@puppetlabs.com>
2018-11-29 11:09:24 -0800

    (maint) Prep 1.9.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/lein-ezbake "1.8.11-SNAPSHOT"
+(defproject puppetlabs/lein-ezbake "1.9.0-SNAPSHOT"
   :description "A system for building packages for trapperkeeper-based applications"
   :url "https://github.com/puppetlabs/ezbake"
   :license {:name "Apache License 2.0"
